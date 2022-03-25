5d618f5d83cffb18f75fc11beba4c6602d7f5d49
Audrey Chenu <97744698+AudreyChenu@users.noreply.github.com>
2022-03-25 09:45:50 +0100

    premier commit todolist

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos:Todo[] = [];
+
+  inputTodo: string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+this.todos = [
+{
+  content: 'Première tâche',
+completed: false},
+{
+  content: 'Seconde tâche',
+completed: false
+},
+
+
+
+]
+
+  }
+
+  toggleDone (id: number){
+
+this.todos.map((v, i) => {
+if (i == id)
+  v.completed = !v.completed;
+   return v;
+  
+
+})
+  }
+
+  deleteTodo (id: number){
+    this.todos = this.todos.filter((v, i ) => i !== id);
+  }
+
+  addTodo () {
+if (this.inputTodo === "") {
+
+  alert("Remplissez le champ")
+}
+
+else {
+    this.todos.push({
+      
+      content: this.inputTodo,
+      completed: false
+    }
+    );
+
+    this.inputTodo = "";
+  }
+}}
